55f5e87b05fe760a5a7aa1a20cb0d5322422da25
bakorx <asnkwamebakor@gmail.com>
2025-08-06 21:28:26 +0000

    complete task 2

--- Task2_ShapeCalculator/ShapeCalculator.cs
+++ Task2_ShapeCalculator/ShapeCalculator.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ShapeAreaCalculator
+{
+    
+    abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    class Circle : Shape
+    {
+        public double Radius { get; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public double Width { get; }
+        public double Height { get; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape myCircle = new Circle(3.5);
+            Shape myRectangle = new Rectangle(5, 4);
+
+            Console.WriteLine("Shape Areas:");
+            Console.WriteLine($"Circle Area: {myCircle.GetArea():F2}");
+            Console.WriteLine($"Rectangle Area: {myRectangle.GetArea():F2}");
+        }
+    }
+}
